5a8c16f4b4ff0d694a66bfa4e852dd2a51a92a25
Administrator <Administrator@DNRTXM720CFMJ14>
2014-10-06 13:03:30 +0800

    主界面的设置

--- neihan/src/com/jt/neihan/MainActivity.java
+++ neihan/src/com/jt/neihan/MainActivity.java
@@ -1,15 +1,47 @@
 package com.jt.neihan;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import com.jt.neihan.fragment.HuoDongFragment;
+import com.jt.neihan.fragment.ImageListFragment;
+import com.jt.neihan.fragment.MineFragment;
+import com.jt.neihan.fragment.ReviewFragment;
+import com.jt.neihan.fragment.TextListFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity implements
+		OnCheckedChangeListener {
+	private RadioGroup radioGroup;
+	private List<Fragment> fragments;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		radioGroup = (RadioGroup) this.findViewById(R.id.main_tab_bar);
+		radioGroup.setOnCheckedChangeListener(this);
+		fragments = new LinkedList<Fragment>();
+		fragments.add(new TextListFragment());
+		fragments.add(new ImageListFragment());
+		fragments.add(new ReviewFragment());
+		fragments.add(new HuoDongFragment());
+		fragments.add(new MineFragment());
+		Fragment fragment = fragments.get(0);
+		FragmentManager manager = getSupportFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		transaction.replace(R.id.main_fragment_framlayout, fragment);
+		transaction.commit();
 	}
 
 	@Override
@@ -17,6 +49,43 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+	}
+
+	@Override
+	public void onCheckedChanged(RadioGroup group, int checkedId) {
+		// TODO Auto-generated method stub
+		int chileCount = group.getChildCount();
+		int checkedIndex = 0;
+		RadioButton btn = null;
+		for (int i = 0; i < chileCount; i++) {
+			btn = (RadioButton) group.getChildAt(i);
+			if (btn.isChecked()) {
+				checkedIndex = i;
+				break;
+			}
+		}
+		Fragment fragment = fragments.get(checkedIndex);
+		FragmentManager manager = getSupportFragmentManager();
+		FragmentTransaction transaction = manager.beginTransaction();
+		transaction.replace(R.id.main_fragment_framlayout, fragment);
+		transaction.commit();
+		// switch (checkedIndex) {
+		// case 0:
+		//
+		// break;
+		// case 1:
+		//
+		// break;
+		// case 2:
+		//
+		// break;
+		// case 3:
+		//
+		// break;
+		// case 4:
+		//
+		// break;
+		// }
 	}
 
 }
